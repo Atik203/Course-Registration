f5c65521b1f9ac0813fd9f798df461aee5d0bdf2
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-15 15:00:02 +0600

    Created Course Cart component

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+const Cart = () => {
+  return (
+    <div className="w-[350px]">
+      <h3 className="text-left text-lg font-bold text-[#2F80ED] mb-8 pt-3">
+        Credit Hour Remaining 20 hr
+      </h3>
+      <hr className="h-[2px] bg-[#1C1B1B33]" />
+      <h3 className="text-[#1C1B1B] font-bold text-lg text-left my-6">
+        Course Name
+      </h3>
+      <hr className="h-[2px] bg-[#1C1B1B33]" />
+    </div>
+  );
+};
+
+export default Cart;
